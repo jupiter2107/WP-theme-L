e1fea42f8f6129d46066edce68ac8bff274d8733
jupiter2107 <s.teramoto.1107@gmail.com>
2014-09-30 02:23:05 +0900

    First Commit to GitHub

---
+++ php_files/single.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ミネルバの徒然なるままに</title>
+
+    <!-- CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/pagetop.css" rel="stylesheet">
+    <link href="/css/header-footer.css" rel="stylesheet">
+    <link href="./style.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link href="/favicon.ico" rel="icon" type="image/vnd.microsoft.icon">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-46761512-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  </head>
+  <body>
+    <div class="container">
+      <div id="banner" class="row">
+        <div class="col-xs-12 col-sm-5 col-md-3">
+          <h1><a href="/"><img src="/img/banner.png" class="img-responsive" alt="ミネルバの徒然なるままに"></a></h1>
+        </div>
+        <div class="col-xs-12 col-sm-7 col-md-9">
+          <p class="text-right">
+            <a href="/blog/"><img src="/img/sites-icon/wordpress.png" alt="ブログ" width="50px"></a>
+            <a href="http://jupiter2107.tumblr.com/"><img src="/img/sites-icon/tumblr.png" alt="tumblr" width="50px"></a>
+            <a href="http://twitter.com/jupiter2107/"><img src="/img/sites-icon/twitter.png" alt="Twitter" width="50px"></a>
+            <a href="http://www.pinterest.com/jupiter2107/"><img src="/img/sites-icon/pinterest.png" alt="Pinterest" width="50px"></a>
+            <a href="https://www.facebook.com/minerva.moe.in"><img src="/img/sites-icon/facebook.png" alt="facebookページ" width="50px"></a>
+            <a href="https://plus.google.com/106096786022158893653"><img src="/img/sites-icon/google+.png" alt="Google+ページ" width="50px"></a>
+          </p>
+        </div>
+      </div><!-- /.Banner -->
+
+      <div id="navbar">
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mmi-navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div class="collapse navbar-collapse" id="mmi-navbar">
+              <ul class="nav navbar-nav">
+                <li><a href="/about/site/">About</a></li>
+                <li><a href="/blog/">Blog</a></li>
+                <li><a href="/anime/">Anime</a></li>
+                <li><a href="/adoption/">Adoption</a></li>
+                <li><a href="/event/">Event</a></li>
+                <li class="dropdown">
+                  <a href="" class="dropdown-toggle" data-toggle="dropdown">Development <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="/new-blog-theme/">New Blog Theme</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="/contact/">Contact</a></li>
+                <li><a href="/sitemap/">Sitemap</a></li>
+              <ul>
+            </div><!-- /.Navbar-collapse -->
+          </div><!-- /.Container-fluid -->
+        </nav>
+      </div><!-- /.Navbar -->
+
+      <div id="info" class="alert alert-info" role="alert">
+        <div class="row">
+          <p class="lead text-center">投稿ページ with WordPress + Bootstrap</p>
+        </div>
+        <div class="row">
+          <div class="col-xs-6 col-sm-6">
+            <a href="/blog/category/production-diary/" class="btn btn-info btn-block" role="button"><span class="octicon octicon-globe"></span> 日誌を読む</a>
+          </div>
+          <div class="col-xs-6 col-sm-6">
+            <a href="./" class="btn btn-info btn-block" role="button"><span class="octicon octicon-globe"></span> 開発トップ</a>
+          </div>
+        </div>
+      </div><!-- /.Info -->
+
+<!-- *************** ▼▼ ここからブログ本体部分 ▼▼ *************** -->
+
+      <!-- Blog navbar -->
+      <div class="blog-navbar" style="padding-bottom: 6px;">
+        <ul class="nav nav-tabs" role="tablist">
+          <li class="active"><a href="#">Blog</a></li>
+          <li><a href="#">Event</a></li>
+          <li><a href="#">Setlist</a></li>
+        </ul>
+      </div>
+
+      <!-- Blog Content -->
+      <div class="row">
+
+        <!-- Main Row -->
+        <div class="col-xs-12 col-sm-8 col-md-9">
+
+          <!-- Post -->
+          <div class="row post">
+            <div class="col-xs-12">
+              <h2><a href="#">記事のタイトルが入ります</a></h2>
+              <p class="post-meta">
+                <span class="post-date">yyyy.mm.dd</span>　
+                <span class="category">Category - <a href="#">カテゴリー</a></span>　
+                <span class="comment-num"><a href="#">Comment - 0</a></span>
+              </p>
+              <p class="text-center"><img src="image/20140718-NngdcEzI.jpg" class="img-responsive"></p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p class="footer-post-meta text-right">
+                <span class="tag">Tag - <a href="#">タグ1</a>、<a href="#">タグ2</a></span>　
+                <span class="author">Author - <a href="#">jupiter2107</a></span>
+              </p>
+            </div>
+          </div><!-- /.Post -->
+
+          <!-- Navigation -->
+          <div class="row blog-nav">
+            <div class="col-xs-6 col-sm-6 col-md-6">
+              <p><a href="#" class="btn btn-default btn-lg btn-block" role="button"><span class="glyphicon glyphicon-chevron-left"></span> NEXT</a></p>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6">
+              <p><a href="#" class="btn btn-default btn-lg btn-block" role="button">PREVIOUS <span class="glyphicon glyphicon-chevron-right"></a></p>
+            </div>
+          </div><!-- /.Navigation -->
+
+          <!-- Comment -->
+          <div class="comment" style="padding: 24px 0px 0px 0px;">
+            <h3>Comment</h3>
+            <ul class="media-list">
+
+              <!-- Comment 1 -->
+              <li class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="mysteryman.jpg" alt="...">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Comment Author <small>- yyyy.mm.dd hh:mm</small></h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                  <!-- Nested Comment 1-1 -->
+                  <div class="media">
+                    <a class="pull-left" href="#">
+                      <img class="media-object" src="mysteryman.jpg" alt="...">
+                    </a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Comment Author <small>- yyyy.mm.dd hh:mm</small></h4>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                      <!-- Nested Comment 1-2 -->
+                      <div class="media">
+                        <a class="pull-left" href="#">
+                          <img class="media-object" src="mysteryman.jpg" alt="...">
+                        </a>
+                        <div class="media-body">
+                          <h4 class="media-heading">Comment Author <small>- yyyy.mm.dd hh:mm</small></h4>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                      </div><!-- /.Nested Comment 1-2 -->
+                    </div>
+                  </div><!-- /.Nested Comment 1-1 -->
+                </div>
+              </li><!-- /.Comment 1 -->
+
+              <!-- Comment 2 -->
+              <li class="media">
+                <a class="pull-right" href="#">
+                  <img class="media-object" src="mysteryman.jpg" alt="...">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Comment Author <small>- yyyy.mm.dd hh:mm</small></h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+              </li><!-- /.Comment 2 -->
+            </ul>
+          </div><!-- /.Comment -->
+
+          <!-- Comment Form -->
+          <div class="comment-form">
+            <h3>Leave a Reply</h3>
+            <p><small>メールアドレスが公開されることはありません。<span class="text-danger">＊</span>がついているものは必須項目です。</small></p>
+            <form class="form-horizontal" role="form">
+              <div class="form-group">
+                <label for="author" class="col-sm-2 control-label">名前<small><span class="text-danger">＊</span></small></label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="author" placeholder="Name">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email" class="col-sm-2 control-label">メールアドレス<small><span class="text-danger">＊</span></small></label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="email" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="url" class="col-sm-2 control-label">ウェブサイト</label>
+                <div class="col-sm-10">
+                  <input type="url" class="form-control" id="url" placeholder="Website">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="comment" class="col-sm-2 control-label">コメント</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" id="comment" rows="3" placeholder="Comment"></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default btn-success">Submit a Comment</button>
+                </div>
+              </div>
+            </form>
+          </div><!--/.Comment Form -->
+        </div><!-- /.Main Row -->
+
+        <!-- Sub Row -->
+        <div class="hidden-xs col-sm-4 col-md-3">
+
+          <!-- Calendar -->
+          <div class="calendar">
+            <h4>Calendar</h4>
+            <h5 class="text-center">2014.07</h5>
+            <table class="table">
+              <tr><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th><th>日</th></tr>
+              <tr><td></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr>
+              <tr><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td></tr>
+              <tr><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td></tr>
+              <tr><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td></tr>
+              <tr><td>28</td><td>29</td><td>30</td><td>31</td><td></td><td></td><td></td></tr>
+            </table>
+          </div>
+
+          <!-- Recent -->
+          <div class="recent">
+            <h4>Recent</h4>
+            <div class="list-group">
+              <a href="#" class="list-group-item"><span class="badge">New!</span> Recent Post 1</a>
+              <a href="#" class="list-group-item">Recent Post 2</a>
+              <a href="#" class="list-group-item">Recent Post 3</a>
+              <a href="#" class="list-group-item">Recent Post 4</a>
+              <a href="#" class="list-group-item">Recent Post 5</a>
+            </div>
+          </div>
+
+          <!-- Category -->
+          <div class="category">
+            <h4>Category</h4>
+            <div class="list-group">
+              <a href="#" class="list-group-item">Category 1</a>
+              <a href="#" class="list-group-item">Category 2</a>
+              <a href="#" class="list-group-item">Category 3</a>
+              <a href="#" class="list-group-item">Category 4</a>
+              <a href="#" class="list-group-item">Category 5</a>
+            </div>
+          </div>
+
+          <!-- Archive -->
+          <div class="archive" style="padding-bottom: 12px;">
+            <h4>Archive</h4>
+            <form name="archive">
+              <select name="archive" class="form-control" onChange="if(document.archive.archive.value){location.href=document.archive.archive.value;}">
+                <option value="#">2014.07</option>
+                <option value="#">2014.06</option>
+                <option value="#">2014.05</option>
+                <option value="#">2014.04</option>
+                <option value="#">2014.03</option>
+                <option value="#">2014.02</option>
+                <option value="#">2014.01</option>
+                <option value="#">2013.12</option>
+                <option value="#">2013.11</option>
+                <option value="#">2013.10</option>
+                <option value="#">2013.09</option>
+                <option value="#">2013.08</option>
+              </select>
+            </form>
+          </div>
+
+          <!-- Tag -->
+          <div class="tag" style="padding-bottom: 5px;">
+            <h4>Tag</h4>
+            <p>
+              <button type="button" class="btn btn-default btn-xs">Tag 01</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 02</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 03</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 04</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 05</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 06</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 07</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 08</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 09</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 10</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 11</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 12</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 13</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 14</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 15</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 16</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 17</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 18</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 19</button>
+              <button type="button" class="btn btn-default btn-xs">Tag 20</button>
+            </p>
+          </div>
+        </div><!-- /.Sub Row -->
+      </div><!-- /.Blog Content -->
+
+<!-- *************** ▲▲ ここまでブログ本体部分 ▲▲ *************** -->
+
+      <div id="footer">
+        <hr>
+        <div id="affiliate">
+          <script type="text/javascript"><!--amazon_ad_tag = "jupiter2107-22"; amazon_ad_width = "728"; amazon_ad_height = "90";//--></script>
+          <script type="text/javascript" src="http://ir-jp.amazon-adsystem.com/s/ads.js"></script>
+        </div>
+        <div id="copyright">
+          <p class="text-center">Copyright &copy; 2009 - <?php echo date('Y')?> <a href="/">minerva.moe.in / ミネルバ @jupiter2107</a> All Rights Reserved.</p>
+        </div>
+      </div><!--/.Footer -->
+    </div><!--/.Container -->
+
+    <div id="back-to-top">
+      <p class="pagetop"><a href="#">▲</a></p>
+    </div><!-- /.Back-to-Top -->
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Plugins -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/pagetop.js"></script>
+  </body>
+</html>
