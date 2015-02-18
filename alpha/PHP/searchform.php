856de110588b5a15e0cf5371e180271b6f410f28
jupiter2107 <s.teramoto.1107@gmail.com>
2015-02-18 21:42:35 +0900

    delete Data

--- alpha/PHP/searchform.php
+++
@@ -1,8 +0,0 @@
-            <form method="get" class="form-inline" role="form" action="<?php echo home_url('/'); ?>">
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-addon"><span class="glyphicon glyphicon-search"></span></div>
-                  <input class="form-control" type="search" placeholder="" name="s" id="s">
-                </div>
-              </div>
-            </form>
